1b8f8eac6be0d81be46af81a1b2419f9a6d86c8f
Akash Gaikwad <akash.gaikwad9945@gmail.com>
2025-02-24 17:44:34 +0530

    correct server file

--- server/src/server.js
+++ server/src/server.js
@@ -1,35 +1,12 @@
-const express = require("express");
 const dotenv = require("dotenv");
-const cors = require("cors");
 const connectDB = require("./config/db.js");
+const app = require("./app");
 
 // Load environment variables
 dotenv.config();
 
 // Connect to MongoDB
 connectDB();
-
-const app = express();
-app.use(express.json());
-app.use(cors());
-
-// Import Routes
-const userRoutes = require("./routes/userRoutes");
-const authRoutes = require("./routes/authRoutes");
-const tradeRoutes = require("./routes/tradeRoutes");
-const marketRoutes = require("./routes/marketInsightsRoutes.js");
-const offerRoutes = require("./routes/offerRoutes");
-const marketInsightsRoutes = require("./routes/marketInsightsRoutes");
-const messageRoutes = require("./routes/messageRoutes");
-
-app.use("/api/users", userRoutes);
-app.use("/api/offers", offerRoutes);
-app.use("/api/auth", authRoutes);
-app.use("/api/trades", tradeRoutes);
-app.use("/api/market", marketRoutes);
-app.use("/api/market-insights", marketInsightsRoutes);
-app.use("/api/messages", messageRoutes);
-
 
 // Start Server
 const PORT = process.env.PORT || 5000;
