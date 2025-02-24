b87fa5d3a3bbe4c982d8010cfb4264e1b3200e85
Akash Gaikwad <akash.gaikwad9945@gmail.com>
2025-02-24 17:49:43 +0530

    correction in trade route

--- server/src/routes/tradeRoutes.js
+++ server/src/routes/tradeRoutes.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const { createTradeOffer, getTradeOffers, updateTradeStatus } = require("../controllers/tradeController");
-const authMiddleware = require("../middleware/authMiddleware");
+const authMiddleware = require("../middlewares/authMiddleware");
+
 
 const router = express.Router();
 
